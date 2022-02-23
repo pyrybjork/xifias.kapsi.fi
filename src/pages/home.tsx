d38bee90d5ff5d695ce22bcdd1e7b85d90a92aec
pyrybjork <pyry.bjork@gmail.com>
2022-02-23 22:07:08 +0200

    started working on router

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <div className="Home">
+        <p>Home</p>
+    </div> 
+  );
+};
+
+export default Home;
