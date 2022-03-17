a4241d6194e34beffe50f88105c9c9f67c17c65e
pyrybjork <pyry.bjork@gmail.com>
2022-03-17 18:00:23 +0200

    added link

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./home.css";
+import { Link } from 'react-router-dom';
+import { FaSnowflake } from 'react-icons/fa';
+import { MdKeyboardArrowRight } from 'react-icons/md';
 
 const Home: React.FunctionComponent = () => {
   return (
     <div className="Home">
-        <p>Home</p>
+        <div className="card">
+          <Link to="/freshsnow" className="link"> Putsku dashboard <MdKeyboardArrowRight className="nav_icon"></MdKeyboardArrowRight> </Link>
+        </div>
     </div> 
   );
 };
