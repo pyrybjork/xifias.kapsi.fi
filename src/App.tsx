8d5937b43c2740f072ae7f70fac07f68a8a1d92f
pyrybjork <pyry.bjork@gmail.com>
2022-04-01 21:58:11 +0300

    footer test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/home";
 import FreshSnowStations from "./pages/fresh_snow_stations";
 import FreshSnowMap from "./pages/fresh_snow_map";
 import Navigation from "./components/navigation";
+import Footer from "./components/footer";
 import AvalancheTerrainMaps from "./pages/avalanche_terrain_maps";
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<p>coming soon</p> } />
+        <Route path='/aboutme' element={<p>työn alla</p> } />
         <Route path='/freshsnow'>
           <Route path='' element={<FreshSnowStations />}/>
           <Route path='map' element={<FreshSnowMap />}/>
@@ -21,6 +22,7 @@ function App() {
         <Route path='/avalanche_terrain_maps' element={<AvalancheTerrainMaps /> } />
         <Route path='*' element={<p>not found</p> } />
       </Routes>
+      {/* <Footer /> */}
     </Router>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+  return (
+    <div className="footer">
+        <hr />
+        pyry.bjork@gmail.com
+    </div>
+  );
+}
+
+export default Footer;
